ae15027fc50f91c01dcfac5b6b54d2aca38ae5ac
rahul.chandna <chandnarahul@yahoo.com>
2019-04-21 10:44:33 +0200

    added code to generate many dinos

--- dino-plot.py
+++ dino-plot.py
@@ -5,7 +5,7 @@ def plotPoints(dino_vectors, scale_x, scale_y):
     dino_sub_length = dino_length - 1
 
     for i in range(dino_length):
-        plot.scatter(dino_vectors[i][0] + scale_x, dino_vectors[i][1] + scale_y)
+        plot.scatter(dino_vectors[i][0] + scale_x, dino_vectors[i][1] + scale_y,s=0.1)
 
     plot.plot([dino_vectors[0][0] + scale_x, dino_vectors[dino_sub_length][0] + scale_x], [dino_vectors[0][1] + scale_y, dino_vectors[dino_sub_length][1] + scale_y])
     for i in range(dino_sub_length):
@@ -27,7 +27,16 @@ axes = figure.add_subplot(1,1,1)
 axes.spines['left'].set_position('center')
 axes.spines['bottom'].set_position('center')
 
+
 plotPoints(dino_vectors,0,0)
-plotPoints(dino_vectors,-1.5,2.5)
+for i in 10,20,30,40,50:
+    plotPoints(dino_vectors,0,i)
+    plotPoints(dino_vectors,0,-i)
+    plotPoints(dino_vectors,i,0)
+    plotPoints(dino_vectors,-i,0)
+    plotPoints(dino_vectors,i,i)
+    plotPoints(dino_vectors,-i,-i)
+    plotPoints(dino_vectors,-i,i)
+    plotPoints(dino_vectors,i,-i)
 
 plot.show()
