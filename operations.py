5f5c8b387453eb93896f95f751ef5cf67f04eadd
rahul chandna <chandnarahul@yahoo.com>
2019-04-29 19:18:52 +0200

    added operation and example to transform dino vectors

--- operations.py
+++ operations.py
@@ -1,10 +1,17 @@
 from math import sqrt
 
+
 def length(v):
     return sqrt(v[0]**2 + v[1]**2)
 
-def subtract(v1,v2):
+
+def subtract(v1, v2):
     return (v1[0] - v2[0], v1[1] - v2[1])
 
-def distance(v1,v2):
-    return length(subtract(v1,v2))
+
+def distance(v1, v2):
+    return length(subtract(v1, v2))
+
+
+def transformFrom(v1, toV2):
+    return (v1[0]+toV2[0], v1[1]+toV2[1])
