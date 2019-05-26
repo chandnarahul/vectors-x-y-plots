3026a9b240b2067ec191a09ba7b526c69f3a60ab
rahul chandna <chandnarahul@yahoo.com>
2019-05-26 15:19:23 +0200

    added multiple 3d points with one 2d point

--- 3d-vectors/2d-vector-in-3d-plot.py
+++ 3d-vectors/2d-vector-in-3d-plot.py
@@ -4,15 +4,14 @@ import matplotlib.pyplot as plot
 figure = plot.figure()
 axes = plot.axes(projection='3d')
 
-axes.scatter3D(10, 10, 0, c=10)
-
-VecStart_x = [0]
-VecStart_y = [0]
-VecStart_z = [0]
-VecEnd_x = [10]
-VecEnd_y = [10]
-VecEnd_z = [0]
-for i in range(1):
+VecStart_x = [0,0,0 ,00,00,00,00,00,00,00,00]
+VecStart_y = [0,0,0 ,00,00,00,00,00,00,00,00]
+VecStart_z = [0,0,0 ,00,00,00,00,00,00,00,00]
+VecEnd_x = [10,20,20,20,20,20,20,20,20,20,20]
+VecEnd_y = [10,20,20,20,20,20,20,20,20,20,20]
+VecEnd_z = [0,20,18 ,16,14,12,10,8,6,4,2]
+for i in range(len(VecEnd_x)):
+    axes.scatter3D(VecEnd_x[i], VecEnd_y[i], VecEnd_z[i], c=10)
     axes.plot([VecStart_x[i], VecEnd_x[i]], [VecStart_y[i],
                                            VecEnd_y[i]], zs=[VecStart_z[i], VecEnd_z[i]])
 plot.show()
